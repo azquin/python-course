6de4f5b5dc5c90c7f0b4a00ba17fabc5a93a4db6
azquin <66213969+azquin@users.noreply.github.com>
2020-06-03 11:40:29 +0200

    Add files via upload

---
+++ cyklus.py
@@ -0,0 +1,26 @@
+from turtle import exitonclick, right,  forward, left, penup, pendown
+
+i = 0 
+
+pendown()
+
+for j in range(6):
+    for i in range(5):
+        forward(75)
+        left(60)
+
+    left(120)
+
+left(120)
+
+for i in range(6):
+    forward(75)
+    left(60)  
+
+
+
+#mám piči to urobit lepšie ....ale nie :D 
+
+
+
+exitonclick()
